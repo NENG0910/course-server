a8fd61cc7cb73c4a2ed7043b08b31f98c032e1b2
NENG0910 <85937816+NENG0910@users.noreply.github.com>
2022-05-31 15:35:12 +0800

    course-model

---
+++ models/course-model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  id: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  instructor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  students: {
+    type: [String],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("Course", courseSchema);
