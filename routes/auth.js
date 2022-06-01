22b9d87eff829a6dc59de5eedf508c68a9fb3143
NENG0910 <85937816+NENG0910@users.noreply.github.com>
2022-06-01 16:46:25 +0800

    register

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
+const res = require("express/lib/response");
+
 const router = require("express").Router();
 const registerValidation = require("../vaildation").registerValidation;
 const loginValidation = require("../vaildation").loginValidation;
+const User = require("../models").userModel;
 
 //有動作即console.log訊息
 router.use((req, res, next) => {
@@ -18,11 +21,32 @@ router.get("/testAPI", (req, res) => {
 });
 
 //postman POST Body raw
-router.post("/register", (req, res) => {
-  console.log("Register");
+router.post("/register", async (req, res) => {
+  //check the validation of data
   const { error } = registerValidation(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
+  }
+  //check the email  exist
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) {
+    return res.status(400).send("This email is registed");
+  }
+
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    role: req.body.role,
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).send({
+      msg: "success",
+      saveObject: savedUser,
+    });
+  } catch (err) {
+    res.status(400).send("User is not been saved.");
   }
 });
 
